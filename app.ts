5aa51d578625235d3f378c8c7b0e3a956daf912d
Jasanhaa <mateuscmtropical@gmail.com>
2022-05-10 22:39:43 -0300

    Estrutura basica

---
+++ app.ts
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors')
+
+var indexRouter = require('./routes'); // asasfdsa
+var usersRouter = require('./routes'); //asdfa
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/tarefas', indexRouter);
+app.use('/usuarios', usersRouter);
+
+module.exports = app;
